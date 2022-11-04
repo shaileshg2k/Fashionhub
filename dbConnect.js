e472d87f3ca2afd0cb1074ccc08ddfe4353bc320
Shailesh <shailugupta2k@gmail.com>
2022-11-04 22:30:35 +0530

    1.0.1

--- dbConnect.js
+++ dbConnect.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose")
 
 mongoose.connect(process.env.DBKEY)
-// mongoose.connect("mongodb://localhost:27017/ducatrestapi")
 .then(()=>console.log("Data Base is Connected"))
 .catch((error)=>console.log(error))
